79b6c5d60b98bbf1db8a0a1c6da64cf0311a7cf0
kc <kcpi9001@yandex.ru>
2019-05-05 15:56:17 +0300

    user input catched and displayed

---
+++ src/components/Issues.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="issue">
+    <p>Leave your issues here, {{ name }} ! Wiu wiu wiu.</p>
+    
+    <br><br><br>
+    <input type="text" placeholder="Enter an issue you have..." v-model="issue" />
+    <div class="">{{ issue }}</div>
+
+    <ul>
+      <li v-for="(data, i) in issues" :key="i">{{ i }}: {{ data.issue }} </li>
+    </ul>
+    <p v-if="issues.length > 1">You have more than 1 issue!</p>
+    <p v-else>You dont overload with the issues.</p>
+    <div v-bind:class="{ alert: !hideAlert, 'funny-class': showFunnyClass }">{{ alert }}</div>
+    <div v-bind:style="{ backgroundColor: bg.color, width: bg.width, height: bg.height }"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Issues',
+  data() {
+    return {
+      name: 'Dickhead',
+      btnState: true,
+      issues: [
+        { issue: 'my husband says it\'s worth' },
+        { issue: 'my mummy never loved me' }
+      ],
+      issue: '',
+      hideAlert: false,
+      showFunnyClass: true,
+      alert: 'My boyfriend alerted me',
+      bg: {
+        color: '#c50f08',
+        width: '100%',
+        height: '30px'
+      }      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style src="./Issues.css" scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home">    
+    <Issues />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Issues from '@/components/Issues.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    Issues
   }
 }
 </script>
